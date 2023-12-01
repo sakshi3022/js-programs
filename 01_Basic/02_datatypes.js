480a54c52b7f5712071e1674ae1a7d8c1883c14f
Sakshi shinde <103052042+sakshi3022@users.noreply.github.com>
2023-12-01 06:30:51 +0000

    coomit

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,27 @@
+
+
+
+
+console.log(3
+     +
+      3
+    )  //code readability should be high
+
+console.log("Sakshi")
+
+let name = "Sakshi"
+let age = 20
+let isLoggedIn = false
+
+// number range = 2 to poewr 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined => value is not assign
+// symbol =>uniqueness
+
+
+// obj 
+
+console.log(typeof "sakshi");
