c478b106d6e3cc30117dd326dfc977a59789416e
Sakshi shinde <103052042+sakshi3022@users.noreply.github.com>
2023-12-03 10:47:39 +0000

    Done

--- 01_Basic/03_converstion.js
+++ 01_Basic/03_converstion.js
@@ -21,8 +21,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "" = false 
 // "hitesh" = true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/*    opertions   */
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 ="Sakshi"
+let str3 = str1 + str2 
+// console.log(str3 + "thank you for login in out page")
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 +2)
+// console.log(1 + 2 + "2")
+
+// console.log((3+4) *5 % 3)
+
+// console.log(+true)
+// console.log(+false)
+
+let gameCounter = 100
+gameCounter++  /*postfix is add after*/
+++gameCounter  /*prefix is add value before*/
+console.log(gameCounter)
