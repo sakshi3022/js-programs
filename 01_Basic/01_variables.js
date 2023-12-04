31d7cae0d9c230a196b96af31033931e8a605fc5
Sakshi shinde <103052042+sakshi3022@users.noreply.github.com>
2023-12-04 07:08:38 +0000

    done

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14456
+let accountEmail = "sakshi23@gmail.com"
+var accountPassword ="12345"
+accountCity = "satara"
+let accountState
+
+// accountId =2
+
+accountEmail ="hc@gmail.com"
+accountPassword = "2333"
+accountCity ="pune"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
